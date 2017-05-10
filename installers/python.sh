98a185b3db0cfbfea9d802633894db037693f06d
Dennis Frett <dennis.frett@guardsquare.com>
2017-05-10 13:42:15 +0200

    Remove unneeded shell scripts.

--- installers/python.sh
+++ installers/python.sh
@@ -5,9 +5,4 @@ sudo pip install --upgrade pip
 
 # Install packages
 sudo pip install gprof2dot
-sudo pip install haxor-news
 sudo pip install mkdocs
-sudo pip install numpy
-sudo pip install pandas
-sudo pip install scipy
-sudo pip install voltron
