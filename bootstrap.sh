d6770c1fc7111290b64dbbea53dab20e2d49e3d9
Dennis Frett <dennis.frett@live.com>
2017-05-11 17:01:35 +0200

    Install plug.vim.

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,7 +27,10 @@ doSync() {
 
 doInstall() {
     info "Installing Extras"
-    
+
+    # Plug.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
     # FZF
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
